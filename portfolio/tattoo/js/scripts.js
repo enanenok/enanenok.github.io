5adfc038eb7bfe6895410dd7d65e621055d32c24
enanenok <enanenok@yandex.ru>
2019-01-06 22:02:56 +0300

    Added project to portfolio

---
+++ portfolio/tattoo/js/scripts.js
@@ -0,0 +1,11 @@
+$(".menu__button").click(function() {
+  $(this).toggleClass("on");
+  $(".nav").slideToggle();
+  return false;
+});
+
+$("a").click(function () {
+  var target = $(this).attr('href');
+  $('html, body').animate({ scrollTop: $(target).offset().top - 45 }, 1300);
+  return false;
+});
