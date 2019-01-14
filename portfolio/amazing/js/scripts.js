2ceb77268e2876ff1e70e4829cbabe873798c0d5
enanenok <enanenok@yandex.ru>
2019-01-14 14:04:40 +0300

    Added new project

---
+++ portfolio/amazing/js/scripts.js
@@ -0,0 +1,14 @@
+/* Third Party Libraries */
+
+/* Custom */
+
+$(".menu-button").click(function() {
+
+  $(this).toggleClass("on");
+
+  $(".menu-box").slideToggle();
+
+  return false;
+
+});
+//# sourceMappingURL=scripts.js.map
